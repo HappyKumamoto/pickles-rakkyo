532dfa762dcde4fb53f07f1591a09b69174b72d8
Happy Kumamoto <zen.word+hub@gmail.com>
2022-05-03 22:28:34 +0900

    culc form

---
+++ cul.js
@@ -0,0 +1,14 @@
+function calc(rakkyo){
+
+  const salt_value=document.getElementById('salt').value=
+  Math.round(rakkyo_value * 0.2);
+
+  const vinegar_value=document.getElementById('vinegar').value=
+  Math.round(rakkyo_value * 0.2);
+
+  const sugar_value=document.getElementById('sudar').value=
+  Math.round(rakkyo_value * 0.2);
+
+  const  volume_value=document.getElementById('volume').value=
+  Math.round(rakkyo_value * 0.2);
+}
