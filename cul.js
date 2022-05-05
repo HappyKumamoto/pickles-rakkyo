351b7b30635671eba9e3c4e48271073bc3c46885
Happy Kumamoto <zen.word+hub@gmail.com>
2022-05-06 02:06:58 +0900

    2nd commit

--- cul.js
+++ cul.js
@@ -1,14 +1,15 @@
-function calc(rakkyo){
+function calc(){
+  let rakkyo_value = document.getElementById('rakkyo');
 
   const salt_value=document.getElementById('salt').value=
   Math.round(rakkyo_value * 0.2);
 
   const vinegar_value=document.getElementById('vinegar').value=
-  Math.round(rakkyo_value * 0.2);
+  Math.round(rakkyo_value * 0.5);
 
-  const sugar_value=document.getElementById('sudar').value=
-  Math.round(rakkyo_value * 0.2);
+  const sugar_value=document.getElementById('sugar').value=
+  Math.round(rakkyo_value * 0.15);
 
   const  volume_value=document.getElementById('volume').value=
-  Math.round(rakkyo_value * 0.2);
+  Math.round(rakkyo_value * 1.8);
 }
