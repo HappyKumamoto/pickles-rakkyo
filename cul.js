208227639a90e91c37834f24b461bee7bb091227
Happy Kumamoto <zen.word+hub@gmail.com>
2022-05-15 15:54:38 +0900

    change font-size

--- cul.js
+++ cul.js
@@ -2,14 +2,16 @@
 
 function calc(rakkyo){
 
-  let salt_value=document.getElementById('salt').value=Math.round(rakkyo * 0.2);
-  console.log(`salt_value`);
+  let salt_value=document.getElementById('salt').value=`${Math.round(rakkyo * 0.2)}g`;
 
-  let vinegar_value=document.getElementById('vinegar').value=Math.round(rakkyo * 0.5);
+  let vinegar_value=document.getElementById('vinegar').value=`${Math.round(rakkyo * 0.5)}ml`;
 
-  let sugar_value=document.getElementById('sugar').value=Math.round(rakkyo * 0.15);
+  let sugar_value=document.getElementById('sugar').value=`${Math.round(rakkyo * 0.15)}g 
+  〜 ${Math.round(rakkyo * 0.3)}g(甘め)`;
 
   let chili_value=document.getElementById('chili').value="お好みで";
 
-  let  volume_value=document.getElementById('volume').value=Math.round(rakkyo * 1.8);
+  let  volume_value=document.getElementById('volume').value=`約 ${Math.round(rakkyo * 1.8)}ml`;
+ 
 }
+
